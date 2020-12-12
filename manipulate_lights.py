1447d52e80ff9225e6b00935221bad0c5b8fafd4
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2020-12-12 19:53:44 +0100

    Add manipulate_lights.py a basic continuous rainbow pattern

---
+++ manipulate_lights.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+#Username and ip adress in seperate files
+with open("username.txt") as f:
+    username = f.read()
+with open("ipadress.txt") as f:
+    ipadress = f.read()
+
+#The id of your chosen light
+light = 5
+
+
+url = "https://" + ipadress + "/api/" + username + "/lights/" + str(light) + "/state"
+
+
+messagebody = {
+    "on":True,
+    "sat":254,
+    "bri":254,
+    "hue":30000
+}
+
+print(json.dumps(messagebody))
+
+
+while True:
+    messagebody["hue"] = (messagebody["hue"]+1000)%65000
+    
+    x = requests.put(url, data = json.dumps(messagebody), verify=False)
+
+    print(x.text)
