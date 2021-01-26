aaea5e598e4807c900e583efecedcd331aca0330
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-01-26 16:54:39 +0100

    Update weather lights to include all yr weather symbols.

--- yr_api.py
+++ yr_api.py
@@ -20,27 +20,60 @@ if len(cmd_args) > 1:
 else:
     light_group = LightGroup(3)
 
-cold = [40000, 50, 254]
-warm = [10000, 150, 254]
+COLD_LIGHT = [40000, 50, 100]
+WARM_LIGHT = [10000, 150, 254]
+NEUTRAL_LIGHT = [10000, 150, 150]
+SUNDOWN_LIGHT = [10000, 150, 1200]
+
+# You can see what each symbol looks like here:
+# https://hjelp.yr.no/hc/en-us/articles/203786121-Weather-symbols-on-Yr
+cold_light_weather = ['fair', 'clearsky']
+
+warm_light_weather = [ 'lightsnowandthunder','lightsnow',
+                       "lightsleetandthunder", 'lightrainandthunder',
+                       'heavysleet', 'lightsleet',
+                       'rainandthunder', 'heavysnowandthunder', 'rain', 'snow',
+                        'cloudy',  'heavyrain', 'lightrain',
+                       'heavysleetandthunder',   'fog', 'sleet',  'heavysnow',
+                       'snowandthunder', 'sleetandthunder',
+                       'heavyrainandthunder']
+
+neutral_light_weather = ['partlycloudy','rainshowers',
+                         'lightssnowshowersandthunder','heavyrainshowers',
+                         'lightssleetshowersandthunder','heavysleetshowers',
+                         'snowshowersandthunder','heavysnowshowers',
+                         'sleetshowersandthunder','lightrainshowersandthunder',
+                         'lightrainshowers','heavyrainshowersandthunder',
+                         'heavysnowshowersandthunder','rainshowersandthunder',
+                         'heavysleetshowersandthunder','sleetshowers',
+                         'lightsleetshowers','lightsnowshowers','snowshowers']
+
+
 while True:
     weather = Yr(location_name="Norway/Trøndelag/Trondheim/Trondheim")
     weather_now = weather.now(as_json=True)
     weather_now = json.loads(weather_now)
+    print(weather_now)
     sunrise_sunset = weather.dictionary["weatherdata"]["sun"]
     sunrise = sunrise_sunset["@rise"]
     sunset = sunrise_sunset["@set"]
     sunrise = datetime.datetime.strptime(sunrise, "%Y-%m-%dT%H:%M:%S")
     sunset = datetime.datetime.strptime(sunset, "%Y-%m-%dT%H:%M:%S")
 
-    if weather_now["symbol"]["@name"] == "Cloudy" or weather_now["symbol"]["@name"] == "snow":
-        light_group.update_values(True, *warm)
-    else:
-        light_group.update_values(True, *cold)
     time_now = datetime.datetime.now()
-    if sunrise < time_now < sunset:
-        light_group.bri = 100
+
+    weather_nom_symbol = weather_now["symbol"]["@name"]
+    if not sunrise < time_now < sunset:
+        light_group.update_values(True, *SUNDOWN_LIGHT)
+    elif weather_nom_symbol in warm_light_weather:
+        light_group.update_values(True, *WARM_LIGHT)
+    elif weather_nom_symbol in cold_light_weather:
+        light_group.update_values(True, *COLD_LIGHT)
+    elif weather_nom_symbol in neutral_light_weather:
+        light_group.update_values(True, *COLD_LIGHT)
     else:
-        light_group.bri = 254
+        print("Unknown weather symbol: ", weather_nom_symbol)
+
     print(light_group.update().text)
 
     time.sleep(600)
